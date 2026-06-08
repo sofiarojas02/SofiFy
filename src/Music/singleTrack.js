cf0548ed644d4a2fc71c388a76b59853392eff06
Sofia Rojas <sofiarojasuptm@gmail.com>
2026-06-07 22:52:09 -0400

    estructura y estilos del reproductor

---
+++ src/Music/singleTrack.js
@@ -0,0 +1,12 @@
+const singleTrack = {
+  id: 1,
+  name: "SoundHelix Song 1",
+  artists: "SoundHelix",
+  image: "https://images.unsplash.com/photo-1614613535308-eb5fbd3d2c17?q=80&w=200&auto=format&fit=crop", // Una carátula de prueba
+  audioUrl: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3" // Audio real libre de derechos
+};
+
+const song = 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3'
+
+
+export {singleTrack, song}
--- src/Reproductive/index.js
+++ src/Reproductive/index.js
@@ -1,7 +1,78 @@
+import './reproductive.css'
+import React from 'react'
+import {ReactComponent as VerifySVG} from './reproductivSVG/verify.svg'
+import {ReactComponent as PauseSVG} from './reproductivSVG/pause.svg'
+import {ReactComponent as PreviousSVG} from './reproductivSVG/previous.svg'
+import {ReactComponent as NextSVG} from './reproductivSVG/next.svg'
+
+import {ReactComponent as ListenSVG} from './reproductivSVG/listen.svg'
+import {ReactComponent as ListSVG} from './reproductivSVG/list.svg'
+import {ReactComponent as MicrophoneSVG} from './reproductivSVG/microphone.svg'
+import {ReactComponent as SpeakerSVG} from './reproductivSVG/speaker.svg'
+
+
 function Reproductive(){
     return(
-        <section className="reproductive__container bg-black">
-            <h1>reproductive__container</h1>
+        <section className=" bg-black ">
+            <div className="reproductive__container d-grid">
+
+                <div className='reproductive reproductive__info'>
+                    <figure className='info__image-container'>
+                        <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTyiqeyfo4W94byruTLSCU8maw187wOqtgPPU-uMu0hN26GVtwbBkuoIXfH&s=10'/>
+                    </figure>
+                    <div>
+                        <h6 className='info--tittle'>Nombre</h6>
+                        <p className='info--subtittle'>Artista</p>
+                    </div>
+                    <div>
+                        <span>
+                            <VerifySVG 
+                            className='verify-svg'
+                            />
+                        </span>
+                    </div>
+                    
+                </div>
+
+                <div className='reproductive reproductive__box'>
+                    <div>
+                        <button className='box__button'>
+                            <PreviousSVG className='box__button--svg'/>
+                        </button>
+                        <button className='box__button bg-white rounded-circle d-flex align-items-center justify-content-center'>
+                            <PauseSVG className='box__button--svg pause-svg'/>
+                        </button>
+                        <button className='box__button'>
+                            <NextSVG className='box__button--svg'/>
+                        </button>
+                    </div>
+
+                    <div>
+                        <p>Reproductor</p>
+                    </div>
+                </div>
+
+                <div className='reproductive reproductive__options'>
+                    <div className='options__buttons'>
+                        <button>
+                            <ListenSVG />
+                        </button>
+                        <button>
+                            <ListSVG />
+                        </button>
+                        <button>
+                            <MicrophoneSVG />
+                        </button>
+                        <button>
+                            <SpeakerSVG />
+                        </button>
+
+                        <button>volumen</button>
+                    </div>
+                </div>
+            </div>
+
+            
         </section>
     )
 }
